00658571625ab044cf450d1277a9f36275c0a880
Narcisa Quinatanilla <narcisaquintanilla25@gmail.com>
2025-06-18 19:38:46 -0500

    estoy creando las preguntas de cada modulo

--- Aplicaciones/core/urls.py
+++ Aplicaciones/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 # views del core
 from .views import core_views
 from .views import crud_administrador_views
+from .views import cuestionario_Modulo1
 
 
 urlpatterns = [
@@ -13,4 +14,6 @@ urlpatterns = [
     path('administrador/', crud_administrador_views.index, name='administrador_index'),
     path('administrador/crear/', crud_administrador_views.create, name='administrador_create'),
     
+    # Cuestionario Modulo 1
+    path('cuestionario_Modulo1/', cuestionario_Modulo1.cuestionario_Modulo1, name='cuestionario_Modulo1'),
 ]
---
+++ Aplicaciones/core/views/cuestionario_Modulo1.py
@@ -0,0 +1,6 @@
+
+from django.shortcuts import render, redirect
+
+
+def cuestionario_Modulo1(request):
+    return render(request, 'cuestionario/cuestionario_Modulo1.html');
