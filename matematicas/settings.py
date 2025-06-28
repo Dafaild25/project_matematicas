1899c706da8e4c92bd9212697c11a7f9e90b17b1
Dafaild <154031943+Dafaild25@users.noreply.github.com>
2025-06-28 18:55:47 -0500

    Desactivando  seguridad

--- matematicas/settings.py
+++ matematicas/settings.py
@@ -246,13 +246,13 @@ else:
     
     
     # Seguridad estricta para producción
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_REFERRER_POLICY = 'same-origin'
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # SESSION_COOKIE_SECURE = True
+    # CSRF_COOKIE_SECURE = True
+    # SECURE_BROWSER_XSS_FILTER = True
+    # SECURE_CONTENT_TYPE_NOSNIFF = True
+    # SECURE_REFERRER_POLICY = 'same-origin'
+    # SECURE_SSL_REDIRECT = True
+    # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
     # Logging simplificado para producción
     LOGGING = {
